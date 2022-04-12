9dd5ba587be19bed0e79be44e6a392b401736b92
Harry <hdhillon478@gmail.com>
2022-04-12 17:21:10 +0100

    Copy auth modal template and export component

--- clip-it/src/app/user/user.module.ts
+++ clip-it/src/app/user/user.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationModalComponent } from './authentication-modal/authentication-modal.component';
 
-
-
 @NgModule({
-  declarations: [
-    AuthenticationModalComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AuthenticationModalComponent],
+  imports: [CommonModule],
+  exports: [AuthenticationModalComponent],
 })
-export class UserModule { }
+export class UserModule {}
