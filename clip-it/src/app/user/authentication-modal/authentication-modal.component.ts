a74b08b334add49d882684045e86915682df2254
Harry <hdhillon478@gmail.com>
2022-04-13 10:10:05 +0100

    add id system to modals

--- clip-it/src/app/user/authentication-modal/authentication-modal.component.ts
+++ clip-it/src/app/user/authentication-modal/authentication-modal.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-authentication-modal',
   templateUrl: './authentication-modal.component.html',
-  styleUrls: ['./authentication-modal.component.css']
+  styleUrls: ['./authentication-modal.component.css'],
 })
 export class AuthenticationModalComponent implements OnInit {
-
-  constructor() { }
+  constructor(public modal: ModalService) {}
 
   ngOnInit(): void {
+    this.modal.register('auth');
+    this.modal.register('test');
   }
-
 }
