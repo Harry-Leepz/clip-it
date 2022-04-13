e8b503ef70f81224f31a29b72d122db3cfbb9d98
Harry <hdhillon478@gmail.com>
2022-04-13 15:21:07 +0100

    generate new component tab in shared

--- clip-it/src/app/shared/shared.module.ts
+++ clip-it/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
   imports: [CommonModule],
   exports: [ModalComponent],
 })
